94f655d0e509f37b1b8cc5f4e5d2ecd0bb7c9d36
Kuroneko-Rares <luigi.galvan@berkeley.edu>
2020-06-18 21:12:20 -0700

    Something about servers

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const e = require("express");
 
 // Sets up the Express App
 const app = express();
@@ -11,82 +12,109 @@ app.use(express.json());
 const reservations = []
 const waitlist = []
 
-app.get("/", (req, res) => 
+app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, "home.html"))
-    );
+);
 
-app.get("/routes/tables", (req, res) => 
+app.get("/routes/tables", (req, res) =>
     res.sendFile(path.join(__dirname, "tables.html"))
-    );
+);
 
-app.get("/routes/reserve", (req, res) => 
+app.get("/routes/reserve", (req, res) =>
     res.sendFile(path.join(__dirname, "reserve.html"))
-    );
+);
 
-app.get("/reservations", function(req, res) {
+app.get("/reservations", function (req, res) {
     return res.json(reservations);
-    });
+});
 
-app.get("/waitlist", function(req, res) {
-        return res.json(waitlist);
-        });
+app.get("/waitlist", function (req, res) {
+    return res.json(waitlist);
+});
+
+app.get("/reservationlength", (req, res) => {
+
+    return reservations.length
+
+})
+
+// app.post("/routes/reserve", function (req, res) {
+//     const newReservation = req.body;
+//     // newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+//     console.log(newReservation);
+//     if (reservations.length < 5) {
+//         reservations.push(newReservation);
+//     }
+//     else {
+//         waitlist.push(newReservation);
+//     }
+//     res.json(newReservation);
+// });
 
 
-app.post("/routes/reserve", function(req, res) {
-    const newReservation = req.body;
-    // newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newReservation);
-    if (reservations.length <= 5){
-        reservations.push(newReservation);
-    }
-    else {
-        waitlist.push(newReservation);
-    }
-    res.json(newReservation);
-    });
 
-
-
-//=======================
+//=======================for tables html
 
 $.ajax({
-    url: "http://localhost:3001/",
+    url: "http://localhost:3001/reservations",
     type: "GET"
 })
-.then(response =>{
-    response.forEach(el => {
-        const newID = el.customerID
-        const newIndex = response.indexOf(el)+1
-        $("#currentRes")
-    });
-})
+    .then(response => {
+        response.forEach(el => {
+            const newID = el.customerID
+            const newIndex = response.indexOf(el) + 1
+            $("#currentRes")
+        });
+    })
 
+$.ajax({
+    url: "http://localhost:3001/waitlist",
+    type: "GET"
+})
+    .then(response => {
+        response.forEach(el => {
+            const newID = el.customerID
+            const newIndex = response.indexOf(el) + 1
+            $("#waitlist")
+        });
+    })
 //========================for reserve html
 
-<script type="text/javascript">
-    $("#add-btn").on("click", function(event) {
-      event.preventDefault();
-      const newReservation = {
-        customerName: $("#customerName").val().trim(),
-        customerEmail: $("#customerEmail").val().trim(),
-        customerID: $("#customerID").val().trim(),
-        phoneNumber: $("#phoneNumber").val().trim()
-      };
+< script type = "text/javascript" >
+    $("#add-btn").on("click", function (event) {
+        event.preventDefault();
+        const newReservation = {
+            customerName: $("#customerName").val().trim(),
+            customerEmail: $("#customerEmail").val().trim(),
+            customerID: $("#customerID").val().trim(),
+            phoneNumber: $("#phoneNumber").val().trim()
+        };
 
-      // Question: What does this code do??
-      $.post("/reservations", newReservation)
-        .then(function(data) {
-          console.log("add.html", data);
-          alert("Adding character...");
-        });
-    });
-  </script>
+        $.ajax({
+            url: "http://localhost:3001/reservationlength",
+            type: "GET"
+        })
+        .then(response => {
+            if (response < 5) {
+                $.post("/reservations", newReservation)
+                    .then(function (data) {
+                        console.log("add.html", data);
+                        alert("Adding reservation...");
+                    });
+            }
 
-
-
+            else {
+                $.post("/waitlist", newReservation)
+                    .then(function (data) {
+                        console.log("add.html", data);
+                        alert("Adding to waitlist...");
+                    });
+            }
+        })
+</script >
 
 
 
 app.listen(PORT, () =>
     console.log("App listening on PORT " + PORT)
-    );
+);
